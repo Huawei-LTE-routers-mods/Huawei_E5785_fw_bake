4e3016443cbc610aac8f5c283a36d083c4050e23
Alexander Bersenev <bay@hackerdom.ru>
2020-03-27 01:32:20 +0500

    fix ttl as 128 by default

--- system/etc/fix_ttl.sh
+++ system/etc/fix_ttl.sh
@@ -3,23 +3,30 @@
 ttl=`cat /system/etc/fix_ttl`
 
 if [ 0$ttl -ne 0 ]; then
-  (xtables-multi iptables -t mangle -C OUTPUT -o eth_x -j TTL --ttl-set 64 || 
-   xtables-multi iptables -t mangle -A OUTPUT -o eth_x -j TTL --ttl-set 64)
+  (xtables-multi iptables -t mangle -C OUTPUT -o eth_x -j TTL --ttl-set 128 || 
+   xtables-multi iptables -t mangle -A OUTPUT -o eth_x -j TTL --ttl-set 128)
   
-  (xtables-multi iptables -t mangle -C POSTROUTING -o eth_x -j TTL --ttl-set 64 ||
-   xtables-multi iptables -t mangle -A POSTROUTING -o eth_x -j TTL --ttl-set 64)
-  
-  (xtables-multi ip6tables -t mangle -C OUTPUT -o eth_x -j HL --hl-set 64 ||
-   xtables-multi ip6tables -t mangle -A OUTPUT -o eth_x -j HL --hl-set 64)
+  (xtables-multi ip6tables -t mangle -C OUTPUT -o eth_x -j HL --hl-set 128 ||
+   xtables-multi ip6tables -t mangle -A OUTPUT -o eth_x -j HL --hl-set 128)
 
-  (xtables-multi ip6tables -t mangle -C POSTROUTING -o eth_x -j HL --hl-set 64 ||
-   xtables-multi ip6tables -t mangle -A POSTROUTING -o eth_x -j HL --hl-set 64)
+  (xtables-multi iptables -t mangle -C POSTROUTING -o eth_x -j TTL --ttl-set 128 ||
+   xtables-multi iptables -t mangle -A POSTROUTING -o eth_x -j TTL --ttl-set 128)
 
+  (xtables-multi ip6tables -t mangle -C POSTROUTING -o eth_x -j HL --hl-set 128 ||
+   xtables-multi ip6tables -t mangle -A POSTROUTING -o eth_x -j HL --hl-set 128)
+
+  (xtables-multi iptables -t mangle -С PREROUTING -i eth_x -m ttl --ttl 1 -j TTL --ttl-inc 4 ||
+   xtables-multi iptables -t mangle -A PREROUTING -i eth_x -m ttl --ttl 1 -j TTL --ttl-inc 4)
+
+  (xtables-multi ip6tables -t mangle -C PREROUTING -i eth_x -m hl --hl-eq 1 -j HL --hl-inc 4 ||
+   xtables-multi ip6tables -t mangle -A PREROUTING -i eth_x -m hl --hl-eq 1 -j HL --hl-inc 4)
 else
-  xtables-multi iptables -t mangle -D OUTPUT -o eth_x -j TTL --ttl-set 64
-  xtables-multi iptables -t mangle -D POSTROUTING -o eth_x -j TTL --ttl-set 64
-  xtables-multi ip6tables -t mangle -D OUTPUT -o eth_x -j HL --hl-set 64
-  xtables-multi ip6tables -t mangle -D POSTROUTING -o eth_x -j HL --hl-set 64
+  xtables-multi iptables -t mangle -D OUTPUT -o eth_x -j TTL --ttl-set 128
+  xtables-multi ip6tables -t mangle -D OUTPUT -o eth_x -j HL --hl-set 128
+  xtables-multi iptables -t mangle -D POSTROUTING -o eth_x -j TTL --ttl-set 128
+  xtables-multi ip6tables -t mangle -D POSTROUTING -o eth_x -j HL --hl-set 128
+  xtables-multi iptables -t mangle -D PREROUTING -i eth_x -m ttl --ttl 1 -j TTL --ttl-inc 4
+  xtables-multi ip6tables -t mangle -D PREROUTING -i eth_x -m hl --hl-eq 1 -j HL --hl-inc 4
 fi
 
 disable_spe=`cat /system/etc/disable_spe`
