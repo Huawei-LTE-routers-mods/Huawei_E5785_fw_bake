2f6625ac23f515b11af4fbe60d938e5ba4b59fab
Alexander Bersenev <bay@hackerdom.ru>
2020-03-29 00:14:14 +0500

    make file and dir mode attributes constants

--- baker.py
+++ baker.py
@@ -1,25 +1,28 @@
 import cpio
 import os
 
+ATTR_IS_FILE = 0o100000
+ATTR_IS_DIR = 0o40000
+
 SYSTEM_FILES_RENAME = [
 
 ]
 
 SYSTEM_FILES = [
-    ("bin/busyboxx", 0, 0, 0o104775),
-    ("bin/xtables-multi", 0, 0, 0o100775),
-    ("xbin/atc", 0, 0, 0o100755),
-    ("xbin/adbd", 0, 0, 0o100755),
-    ("xbin/dropbear", 0, 0, 0o100755),
-    ("xbin/sftp-server", 0, 0, 0o100755),
-    ("xbin/scp", 0, 0, 0o100755),
-    ("xbin/balong-nvtool", 0, 0, 0o100755),
-    ("xbin/imei_generator", 0, 0, 0o100755),
-    ("xbin/tinyproxy", 0, 0, 0o100755),
-    ("etc/fix_ttl.sh", 0, 0, 0o100755),
-    ("etc/fix_ttl", 0, 0, 0o100774),
-    ("etc/disable_spe", 0, 0, 0o100774),
-    ("etc/autorun.sh", 0, 0, 0o100500),
+    ("bin/busyboxx", 0, 0, ATTR_IS_FILE | 0o4775),
+    ("bin/xtables-multi", 0, 0, ATTR_IS_FILE | 0o775),
+    ("xbin/atc", 0, 0, ATTR_IS_FILE | 0o755),
+    ("xbin/adbd", 0, 0, ATTR_IS_FILE | 0o755),
+    ("xbin/dropbear", 0, 0, ATTR_IS_FILE | 0o755),
+    ("xbin/sftp-server", 0, 0, ATTR_IS_FILE | 0o755),
+    ("xbin/scp", 0, 0, ATTR_IS_FILE | 0o755),
+    ("xbin/balong-nvtool", 0, 0, ATTR_IS_FILE | 0o755),
+    ("xbin/imei_generator", 0, 0, ATTR_IS_FILE | 0o755),
+    ("xbin/tinyproxy", 0, 0, ATTR_IS_FILE | 0o755),
+    ("etc/fix_ttl.sh", 0, 0, ATTR_IS_FILE | 0o755),
+    ("etc/fix_ttl", 0, 0, ATTR_IS_FILE | 0o774),
+    ("etc/disable_spe", 0, 0, ATTR_IS_FILE | 0o774),
+    ("etc/autorun.sh", 0, 0, ATTR_IS_FILE | 0o500),
 ]
 
 APP_FILES_RENAME = [
@@ -28,24 +31,24 @@ APP_FILES_RENAME = [
 ]
 
 APP_FILES = [
-    ("config/wifi/countryChannel.xml", 1000, 1000, 0o100775),
-    ("bin/device", 1000, 1000, 0o100775),
-    ("bin/oled", 1000, 1000, 0o100775),
-    ("oled_hijack", 1000, 1000, 0o40755),
-    ("oled_hijack/oled_hijack.so", 1000, 1000, 0o100775),
-    ("oled_hijack/web_hook.so", 1000, 1000, 0o100775),
-    ("oled_hijack/web_hook_client", 1000, 1000, 0o100775),
-    ("oled_hijack/ttl_and_imei.sh", 1000, 1000, 0o100775),
-    ("oled_hijack/no_battery_mode.sh", 1000, 1000, 0o100775),
-    ("oled_hijack/radio_mode.sh", 1000, 1000, 0o100775),
-    ("oled_hijack/user_scripts.sh", 1000, 1000, 0o100775),
-    ("prometheus", 1000, 1000, 0o40755),
-    ("prometheus/tinyproxy.conf", 1000, 1000, 0o100775),
-    ("prometheus/start_exporter.sh", 1000, 1000, 0o100775),
-    ("prometheus/httpd_root", 1000, 1000, 0o40755),
-    ("prometheus/httpd_root/index.html", 1000, 1000, 0o100664),
-    ("prometheus/httpd_root/cgi-bin", 1000, 1000, 0o40755),
-    ("prometheus/httpd_root/cgi-bin/prometheus.cgi", 1000, 1000, 0o100775),
+    ("config/wifi/countryChannel.xml", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("bin/device", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("bin/oled", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("oled_hijack", 1000, 1000, ATTR_IS_DIR | 0o755),
+    ("oled_hijack/oled_hijack.so", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("oled_hijack/web_hook.so", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("oled_hijack/web_hook_client", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("oled_hijack/ttl_and_imei.sh", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("oled_hijack/no_battery_mode.sh", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("oled_hijack/radio_mode.sh", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("oled_hijack/user_scripts.sh", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("prometheus", 1000, 1000, ATTR_IS_DIR | 0o755),
+    ("prometheus/tinyproxy.conf", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("prometheus/start_exporter.sh", 1000, 1000, ATTR_IS_FILE | 0o775),
+    ("prometheus/httpd_root", 1000, 1000, ATTR_IS_DIR | 0o755),
+    ("prometheus/httpd_root/index.html", 1000, 1000, ATTR_IS_FILE | 0o664),
+    ("prometheus/httpd_root/cgi-bin", 1000, 1000, ATTR_IS_DIR | 0o755),
+    ("prometheus/httpd_root/cgi-bin/prometheus.cgi", 1000, 1000, ATTR_IS_FILE | 0o775),
 
 ]
 
